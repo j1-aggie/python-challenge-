3a298fc4174e026f65febe1f617d2420442b75b7
Jeremy jones <j1-aggie@hotmail.com>
2020-08-18 13:21:56 -0500

    went with a different approach to loop in the csv after researching class notes

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,6 +5,29 @@ import os
 # module for reading csv file
 import csv
 
-csvpath = os.path.join('..', 'Resources', 'BudgetData.csv')
+# Set Variables
+Total_Months = 0
+Net_Profit = 0
+Greatest_Increase_Profits = 0
+Greatest_Decrease-Losses = 0
+Month_changes = []
+Avg_Changes = []
+Date = []
 
-with open(csvpath, 'r') as csvfile:
+
+
+# set the path for the data file
+PyBankcsv = os.path.join('Resources', 'Budget_Data.csv')
+
+# reading the csv
+
+with open(PyBankcsv, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+
+    for row in CSVreader:
+        print(row)
+        print(row[0])
+        print(row[0],row[1],row[2],)
+
+    
