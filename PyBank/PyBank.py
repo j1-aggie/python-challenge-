74c700d418bebe48b4cc8c28ff45d36b83ccde19
Jeremy jones <j1-aggie@hotmail.com>
2020-08-20 11:17:08 -0500

    saving changes

--- PyBank/PyBank.py
+++ PyBank/PyBank.py
@@ -14,8 +14,8 @@ Net_Profit = 0
 Greatest_Increase_Profits = 0
 Greatest_Decrease_Losses = 0
 Monthly_changes = []
-Greatest_Month_Increase = []
-Greatest_Decrease_Month = []
+Greatest_Month_Increase = 0
+Greatest_Month_Decrease = 0
 CountofMonths = []
 
 
@@ -27,27 +27,71 @@ with open(PyBankcsv, newline='') as csvfile:
     csv_header = next(csvreader)
     row = next(csvreader)
 
-
 # calculate total number of months
-previous_row = int(row[1])
-Total_Months += 1
+    previous_row = int(row[1])
+    
+    Total_Months += 1
 
 # Amount of Profit/Losses - Net
-Net_Profit += int(row[1])
+    Net_Profit += int(row[1])
 
-# Greatest increase 
-Greatest_Increase_Profits = int(row[1])
-
-# Greatest increase monthly
-Greatest_Month_Increase = row[0]
-
-# read each row after header
-    for row in csvreader:
+# Setting variables for rows
+    Greatest_Increase_Profits = int(row[1])
+    Greatest_Month_Increase = row[0]
     
-        # number of months in data
-        Total_Months += 1 
-        
+
+# read each row
+for row in csvreader:
+
+    Total_Months += 1
+    net_amount += int(row[1])
+
+    # change from month to month
+    change_revenue = int(row[1]) - previous_row
+    Monthly_changes.append(revenue_change)
+    previous_row = int(row[1])
+    CountofMonths.append(row[0])
+
+    # greatest increase
+    if int(row[1]) > Greatest_Increase_Profits:
+        Greatest_Increase_Profits = int(row[1])
+        Greatest_Month_Increase = row[0]
+
+
+    # greatest decrease
+    if int(row[1]) < Greatest_Decrease_Losses:
+        Greatest_Decrease_Losses = int(row[1])
+        Greatest_Month_Decrease = row[0]
 
     
-# Set variable for output 
-output_file = os.path.join("Final_Analysis.txt")
+
+# average change with date
+average_change = sum(Monthly_changes)/len(Monthly_changes)
+
+highest = max(Monthly_changes)
+lowest = min(Monthly_changes)
+
+# output
+    
+#file to write to   
+output_file = os.path.join("Final_Analysis_PyBank.txt")
+
+
+
+# print out
+print(f"Financial Analysis")
+print(f"-----------------------------------------")
+print(f"Total Months: {Total_Months}")
+print(f"Total: ${net_amount}")
+print(f"Average Change: ${average_change:.2f}")
+print(f"Greatest Increase in Profits:, {Greatest_Month_Increase},(${highest})")
+print(f"Greatest Decrease in Profits:, {Greatest_Month_Decrease}, (${lowest})")
+
+# write new data
+txtfile.write(f"Financial Analysis\n")
+txtfile.write(f"-----------------------------------\n")
+txtfile.write(f"Total Months: ${Total_Months}\n")
+txtfile.write(f"Total: ${net_amount}\n")
+txtfile.write(f"Average Change: ${average_change}\n")
+txtfile.write(f"Greatest Increase in Profits:, {Greatest_Month_Increase}, (${highest})\n")
+txtfile.write(f"Greatest Decrease in Profits:, {Greatest_Month_Decrease}, (${lowest})\n")
