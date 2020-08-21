f6978c90db397f62e767ff469fe956f279a83573
Jeremy jones <j1-aggie@hotmail.com>
2020-08-21 13:22:20 -0500

    saving

--- PyPoll/PyPoll.py
+++ PyPoll/PyPoll.py
@@ -24,8 +24,34 @@ with open(PyPollcsv, newline='') as csvfile:
     csv_header = next(csvreader)
     row = next(csv reader)
 
-    
+    # read each row of data 
+    for row in csvreader:
 
-    Total_Votes += 1
+        # find total votes
+
+        Total_Votes += 1
+
+        # find the list of candidates
+        if row[2] in candidates.keys():
+            candidates[row[2]] += 1
+        else:
+            candidates[rows[2]] = 1
+
+        # defining the candidates dict
+        for key, value in candidates.items():
+            candidates_percent[key]=round((value/Total_Votes)*100,1)
+        # finding the winner using dict
+        for key in candidates.keys():
+            if candidates[key] > votes_won:
+                winner = key
+                votes_won = candidates[key]
+
+# print outs
+
+print("Election Results")
+print("------------------------------------------------")
+print("Total Votes: " + str(Total_Votes))
+print("------------------------------------------------")
+print(key + )
 
 
