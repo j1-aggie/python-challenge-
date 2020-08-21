50e123cb1e06b9e1f6eb818ef9f96c97273a0b6f
Jeremy jones <j1-aggie@hotmail.com>
2020-08-21 13:02:18 -0500

    writing if statement

--- PyPoll/PyPoll.py
+++ PyPoll/PyPoll.py
@@ -1,3 +1,31 @@
 # pypoll
+# import the os module
+import os
 
-print(test)
+# module for reading csv file
+import csv
+
+# set the path for the data file
+PyPollcsv = os.path.join('.', 'Resources', 'election_data')
+
+# Set Variables
+Total_Votes = 0
+candidates = {}
+candidates_percent_vote = {}
+votes_won = 0
+winner_name = ""
+
+
+# open and read the csv
+with open(PyPollcsv, newline='') as csvfile:
+    # reader specifies delimiter
+    csvreader = csv.reader(csvfile, delimiter=',')
+    # read header row first
+    csv_header = next(csvreader)
+    row = next(csv reader)
+
+    
+
+    Total_Votes += 1
+
+
